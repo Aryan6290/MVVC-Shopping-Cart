d6ce9a912963ab98473e19a120c05575ced84070
Aryan Barnwal <aryanbarnwal007@gmail.com>
2021-11-05 20:33:04 +0530

    Init

---
+++ app/src/main/java/com/example/mvvm_shopping_cart/views/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mvvm_shopping_cart.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvvm_shopping_cart.R
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
