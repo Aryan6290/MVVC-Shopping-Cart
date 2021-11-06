4a8d5b05b7f34bff6890948ec1f2180ba1eb92cb
Aryan Barnwal <aryanbarnwal007@gmail.com>
2021-11-06 20:22:25 +0530

    Added navigation and fragments

---
+++ app/src/main/java/com/example/mvvm_shopping_cart/repositories/ShopRepo.kt
@@ -0,0 +1,4 @@
+package com.example.mvvm_shopping_cart.repositories
+
+class ShopRepo {
+}
---
+++ app/src/main/java/com/example/mvvm_shopping_cart/viewModels/ShopViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.mvvm_shopping_cart.viewModels
+
+import androidx.lifecycle.ViewModel
+
+class ShopViewModel : ViewModel() {
+
+}
